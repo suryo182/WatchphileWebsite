9ce67cf5359b552aff7eea35117d6a0dcece7ec2
gokhan alici <gokhanaliccii@gmail.com>
2022-02-05 11:10:14 +0000

    Add ghost dependency

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,14 +13,22 @@ module.exports = {
   plugins: [
     `gatsby-plugin-postcss`,
     `gatsby-plugin-react-helmet`,
-  {resolve: `gatsby-plugin-sitemap`},
-  {
-    resolve: `gatsby-plugin-manifest`,
-    options: {
-      icon: `src/images/favicon_big.png`,
-      name: "Watchphile",
-      short_name: "Watchphile",
+    { resolve: `gatsby-plugin-sitemap` },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        icon: `src/images/favicon_big.png`,
+        name: "Watchphile",
+        short_name: "Watchphile",
+      }
+    },
+    {
+      resolve: `gatsby-source-ghost`,
+      options: {
+        apiUrl: `http://3.229.153.93`,
+        contentApiKey: `6bc575c4927ff67999516fb28f`
+      }
     }
-  }
-],
+
+  ],
 }
